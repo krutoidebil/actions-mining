585bf365791534d0448eb00ad12411496c1199e3
httsmvkcom <alenyshka81ivanyshka@gmail.com>
2021-02-26 14:59:47 +0300

    Add files via upload

---
+++ Minimal_PC_Miner.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import socket, hashlib, urllib.request, time, os, sys
+
+soc = socket.socket()
+soc.settimeout(10)
+
+username = "httsmvkcom"
+UseLowerDiff = False 
+
+while True:
+    try:
+        serverip = "https://mhcoin.s3.filebase.com/Pool.txt"  # Serverip file
+        with urllib.request.urlopen(serverip) as content:
+            content = (
+                content.read().decode().splitlines()
+            )  
+        pool_address = content[0] 
+        pool_port = content[1] 
+
+        soc.connect((str(pool_address), int(pool_port)))
+        server_version = soc.recv(3).decode()
+        print("Версия сервера:", server_version)
+        while True:
+            if UseLowerDiff:
+                soc.send(
+                    bytes("JOB," + str(username) + ",MEDIUM", encoding="utf8")
+                )  
+            else:
+                soc.send(
+                    bytes("JOB," + str(username), encoding="utf8")
+                )  
+            job = soc.recv(1024).decode()  
+            job = job.split(",")  
+            difficulty = job[2]
+
+            for result in range(
+                100 * int(difficulty) + 1
+            ): 
+                ducos1 = hashlib.sha1(
+                    str(job[0] + str(result)).encode("utf-8")
+                ).hexdigest()
+                if job[1] == ducos1:
+                    soc.send(
+                        bytes(str(result) + ",,Minimal_PC_Miner", encoding="utf8")
+                    )  
+                    feedback = soc.recv(1024).decode()
+                    if feedback == "GOOD":
+                        print("[Accepted!] Решение:", result, "Сложность:", difficulty)
+                        break
+                    elif feedback == "BAD":
+                        print("[Rejected!!!] Решение:", result, "Сложность:", difficulty)
+                        break
+    except Exception as e:
+        os._exit(1)
